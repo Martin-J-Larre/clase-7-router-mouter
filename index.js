d3eb3b169455c89dc5bb4aec7c5485bad2bd3ed1
Martín Javier Larré <75001420+Martin-J-Larre@users.noreply.github.com>
2021-11-06 00:15:30 -0400

    Ejercicio 4

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const path = require('path');
 const PORT = 8088;
 
 app.use("/files",express.static(path.join(__dirname, "uploads")));
+app.use("/html",express.static(path.join(__dirname, "wiews")));
 app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res) =>{
-    console.log("En la raiz del server");
-    res.send(true);
-});
+// app.get("/", (req, res) =>{
+//     console.log("En la raiz del server");
+//     res.send(true);
+// });
 
 serverRouter(app);
 
